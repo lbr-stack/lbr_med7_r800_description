7642e5c27f7833b1e754a281e99d45e913a39155
mhubii <m.huber_1994@hotmail.de>
2026-01-30 10:52:22 +0000

    added view robot launch sample

---
+++ README.md
@@ -0,0 +1,8 @@
+# KUKA LBR Med 7 Description
+Launch an example using
+
+```shell
+ros2 launch \
+    med7_description \
+    view_robot.launch.py
+```
---
+++ launch/view_robot.launch.py
@@ -0,0 +1,63 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description() -> LaunchDescription:
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                name="robot_name",
+                default_value="lbr",
+                description="The robot's name. Links in the tf tree will be prefixed as <robot_name>_link. Same applies to joints.",
+            ),
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                name="robot_state_publisher",
+                output="screen",
+                parameters=[
+                    {
+                        "robot_description": Command(
+                            [
+                                FindExecutable(name="xacro"),
+                                " ",
+                                PathSubstitution(FindPackageShare("med7_description"))
+                                / "urdf"
+                                / "med7.urdf.xacro",
+                                " robot_name:=",
+                                LaunchConfiguration("robot_name"),
+                            ]
+                        )
+                    },
+                ],
+            ),
+            Node(
+                package="joint_state_publisher_gui",
+                executable="joint_state_publisher_gui",
+                name="joint_state_publisher_gui",
+                output="screen",
+            ),
+            Node(
+                package="rviz2",
+                executable="rviz2",
+                name="rviz2",
+                output="screen",
+                arguments=[
+                    [
+                        "-d",
+                        PathSubstitution(FindPackageShare("med7_description"))
+                        / "rviz"
+                        / "view_robot.rviz",
+                    ]
+                ],
+            ),
+        ]
+    )
